020423dbfeeff2963ccc62e472637eaa1fa31c6f
ibrohimjonkurbanov <ibrohimjonteamwork@gmail.com>
2023-01-17 20:14:56 +0500

    js

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// const myArray = [ [false, true] , [1, 2], 99 ,"yaxshi"]
+
+//  for (let i =0; i < myArray.length; ++i)
+// {
+//       if (typeof myArray [i] == 'string')
+//       {
+//          console.log("INDEX:", i)
+//       }
+// }
+
+
+// const data = [ [1, 2, 3], "qizil", [4,5,6], true, false]
+// const newData = []
+//    for ( let i = 0 ; i < data.length; i++)
+//    {
+//     if (Array.isArray(data[i]))
+//        {
+//         for(let k = 0; k < data[i].length; k++)
+//         {
+//             newData.push(data[i][k])
+//         }
+//        }
+//    }
+
+//  console.log(newData)
+
+//  const falsey = [true, '11', null, 0, "", false, undefined, NaN, 1, "Yaxshi"]
+//  const newFalsey = [] 
+//   for (let i = 0; i < falsey.length; i++)
+//   {
+//     if(!falsey[i])
+//     {
+//       newFalsey.push(falsey[i])
+//     }
+//   }
+   
+//   console.log(newFalsey)
+
+
+// const word = 'samarqand'
+// let newWord = ''
+
+// const wordSplit = word.split('').reverse()
+// console.log(newWord)
+
+
+const text = 'assalomu alaykum Yaxshimisiz'
+const textNew = ''
+
+for(let i = 0; i <  text.length; i++)
+{
+}
+
+
+console.log(textNew.split(' '))
+
+
